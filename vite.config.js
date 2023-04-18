ec1771407c2a8f6aa79c00bda8e97ce74ef7aea3
tfkhdyt <tfkhdyt@proton.me>
2023-04-18 07:26:52 +0700

    feat: add popular section

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import UnoCSS from "unocss/vite";
-import { defineConfig } from "vite";
+import UnoCSS from 'unocss/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [UnoCSS()],
+	plugins: [UnoCSS()],
 });
